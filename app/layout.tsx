9c8a6b2f99c5f98db51f6e9b9bb012fcbfeb6693
dismaying <149173124+dismaying@users.noreply.github.com>
2025-01-20 06:46:09 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,12 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        {/* Google Fonts */}
+        {/* Correct usage of crossOrigin */}
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
           rel='preconnect'
           href='https://fonts.gstatic.com'
-          crossOrigin='true'
+          crossOrigin='anonymous'
         />
         <link
           href='https://fonts.googleapis.com/css2?family=Overlock:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap'
